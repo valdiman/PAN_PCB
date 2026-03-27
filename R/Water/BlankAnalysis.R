ceef81078e69b9144246ac8db11ba8b0959a9621
Martinez Araneda <martie@uiowa.edu>
2026-03-27 09:52:21 -0500

    updated version.

--- R/Water/BlankAnalysis.R
+++ R/Water/BlankAnalysis.R
@@ -1,14 +1,22 @@
 # Initial code to check PAN PCB data in water
 
 
+# Install Packages
+{
+  install.packages("dplyr")
+  install.packages("ggplot2")
+}
 
-# load library
-library(dplyr)
+# Load Libraries
+{
+  library(dplyr)
+  library(ggplot2)
+}
 
 # Read data ---------------------------------------------------------------
 pan_water <- read.csv("Data/Water/data.csv")
 
-# Blanks
+# Blank Analysis ----------------------------------------------------------
 pan_water_blank <- pan_water %>%
   filter(experiment == "blank")
 
@@ -62,24 +70,25 @@ shapiro.test(log10(pan_water_blank[, 12]))$p.value
 
 # Calculate LOQ of the log10
 # Upper 95 CI% (=mean + 1.96*sd/(n)^0.5)
-log_PCB4 <- log10(pan_water_blank$PCB4_ng_g)
-n <- sum(!is.na(log_PCB4))
-loq_log4 <- mean(log_PCB4, na.rm = TRUE) +
-  1.96 * sd(log_PCB4, na.rm = TRUE) / sqrt(n)
-loq_4 <- 10^loq_log4
+log_PCB4_bl <- log10(pan_water_blank$PCB4_ng_g)
+n <- sum(!is.na(log_PCB4_bl))
+loq_log4_bl <- mean(log_PCB4_bl, na.rm = TRUE) +
+  1.96 * sd(log_PCB4_bl, na.rm = TRUE) / sqrt(n)
+loq_4 <- 10^loq_log4_bl
 
-log_PCB18 <- log10(pan_water_blank$PCB18_ng_g)
-n <- sum(!is.na(log_PCB18))
-loq_log18 <- mean(log_PCB18, na.rm = TRUE) +
-  1.96 * sd(log_PCB18, na.rm = TRUE) / sqrt(n)
-loq_18 <- 10^loq_log18
+log_PCB18_bl <- log10(pan_water_blank$PCB18_ng_g)
+n <- sum(!is.na(log_PCB18_bl))
+loq_log18_bl <- mean(log_PCB18_bl, na.rm = TRUE) +
+  1.96 * sd(log_PCB18_bl, na.rm = TRUE) / sqrt(n)
+loq_18 <- 10^loq_log18_bl
 
-log_PCB52 <- log10(pan_water_blank$PCB52_ng_g)
-n <- sum(!is.na(log_PCB52))
-loq_log52 <- mean(log_PCB52, na.rm = TRUE) +
-  1.96 * sd(log_PCB52, na.rm = TRUE) / sqrt(n)
-loq_52 <- 10^loq_log52
+log_PCB52_bl <- log10(pan_water_blank$PCB52_ng_g)
+n <- sum(!is.na(log_PCB52_bl))
+loq_log52_bl <- mean(log_PCB52_bl, na.rm = TRUE) +
+  1.96 * sd(log_PCB52_bl, na.rm = TRUE) / sqrt(n)
+loq_52 <- 10^loq_log52_bl
 
+# Sample Analysis ---------------------------------------------------------
 # Retrieve samples
 pan_water_samples <- pan_water %>%
   filter(experiment != "blank")
@@ -89,4 +98,74 @@ pan_water_samples$PCB18_ng_g <- pan_water_samples$PCB18 / pan_water_samples$mass
 pan_water_samples$PCB52_ng_g <- pan_water_samples$PCB52 / pan_water_samples$mass
 
 # Comparison between samples and loq @ log10 scale
+# PCB4
+log_PCB4_sa <- log10(pan_water_samples$PCB4_ng_g)
+pan_water_samples$PCB4_ng_g_loq_co <- ifelse(
+  is.na(log_PCB4_sa),
+  NA,
+  ifelse(log_PCB4_sa > loq_log4_bl, 10^(log_PCB4_sa), NA)
+)
+
+# PCB18
+log_PCB18_sa <- log10(pan_water_samples$PCB18_ng_g)
+pan_water_samples$PCB18_ng_g_loq_co <- ifelse(
+  is.na(log_PCB18_sa),
+  NA,
+  ifelse(log_PCB18_sa > loq_log18_bl, 10^(log_PCB18_sa), NA)
+)
+
+# PCB52
+log_PCB52_sa <- log10(pan_water_samples$PCB52_ng_g)
+pan_water_samples$PCB52_ng_g_loq_co <- ifelse(
+  is.na(log_PCB52_sa),
+  NA,
+  ifelse(log_PCB52_sa > loq_log52_bl, 10^(log_PCB52_sa), NA)
+)
+
+# Plots
+# Shaking
+ggplot(
+  pan_water_samples %>% filter(experiment == "shaking"),
+  aes(x = time, y = PCB4_ng_g_loq_co, color = SolutionPCB)
+) +
+  geom_point() +
+  facet_wrap(~ SolutionPCB)
+
+ggplot(
+  pan_water_samples %>% filter(experiment == "shaking"),
+  aes(x = time, y = PCB18_ng_g_loq_co, color = SolutionPCB)
+) +
+  geom_point() +
+  facet_wrap(~ SolutionPCB)
+
+ggplot(
+  pan_water_samples %>% filter(experiment == "shaking"),
+  aes(x = time, y = PCB52_ng_g_loq_co, color = SolutionPCB)
+) +
+  geom_point() +
+  facet_wrap(~ SolutionPCB)
+
+# No-shaking
+ggplot(
+  pan_water_samples %>% filter(experiment == "no_shaking"),
+  aes(x = time, y = PCB4_ng_g_loq_co, color = SolutionPCB)
+) +
+  geom_point() +
+  facet_wrap(~ SolutionPCB)
+
+ggplot(
+  pan_water_samples %>% filter(experiment == "no_shaking"),
+  aes(x = time, y = PCB18_ng_g_loq_co, color = SolutionPCB)
+) +
+  geom_point() +
+  facet_wrap(~ SolutionPCB)
+
+ggplot(
+  pan_water_samples %>% filter(experiment == "no_shaking"),
+  aes(x = time, y = PCB52_ng_g_loq_co, color = SolutionPCB)
+) +
+  geom_point() +
+  facet_wrap(~ SolutionPCB)
+
+
 
